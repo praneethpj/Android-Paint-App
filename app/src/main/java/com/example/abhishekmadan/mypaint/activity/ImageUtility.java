bc273881df622a603d2ed7a021c9b0c2bbe623a1
praneethpj <pubudupraneeth@gmail.com>
2021-10-08 06:48:50 +0530

    need to improve

---
+++ app/src/main/java/com/example/abhishekmadan/mypaint/activity/ImageUtility.java
@@ -0,0 +1,168 @@
+package com.example.abhishekmadan.mypaint.activity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.media.ExifInterface;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class ImageUtility{
+
+
+    public static byte[] compressBitmap(String file, int width, int height, int maxSizeBytes) {
+        BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
+        bmpFactoryOptions.inJustDecodeBounds = true;
+        Bitmap bitmap;
+
+        int heightRatio = (int) Math.ceil(bmpFactoryOptions.outHeight / (float) height);
+        int widthRatio = (int) Math.ceil(bmpFactoryOptions.outWidth / (float) width);
+
+        if (heightRatio > 1 || widthRatio > 1) {
+            if (heightRatio > widthRatio) {
+                bmpFactoryOptions.inSampleSize = heightRatio;
+            } else {
+                bmpFactoryOptions.inSampleSize = widthRatio;
+            }
+        }
+
+        bmpFactoryOptions.inJustDecodeBounds = false;
+        bitmap = BitmapFactory.decodeFile(file, bmpFactoryOptions);
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        int currSize;
+        int currQuality = 100;
+
+        do {
+            bitmap.compress(Bitmap.CompressFormat.JPEG, currQuality, stream);
+            currSize = stream.toByteArray().length;
+            // limit quality by 5 percent every time
+            currQuality -= 5;
+
+        } while (currSize >= maxSizeBytes);
+
+        return stream.toByteArray();
+    }
+
+
+
+
+    public static ImageUtility mInstant;
+
+    public static ImageUtility getInstant(){
+        if(mInstant==null){
+            mInstant = new ImageUtility();
+        }
+        return mInstant;
+    }
+
+    public  Bitmap getCompressedBitmap(String imagePath) {
+        float maxHeight = 1920.0f;
+        float maxWidth = 1080.0f;
+        Bitmap scaledBitmap = null;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        Bitmap bmp = BitmapFactory.decodeFile(imagePath, options);
+
+        int actualHeight = options.outHeight;
+        int actualWidth = options.outWidth;
+        float imgRatio = (float) actualWidth / (float) actualHeight;
+        float maxRatio = maxWidth / maxHeight;
+
+        if (actualHeight > maxHeight || actualWidth > maxWidth) {
+            if (imgRatio < maxRatio) {
+                imgRatio = maxHeight / actualHeight;
+                actualWidth = (int) (imgRatio * actualWidth);
+                actualHeight = (int) maxHeight;
+            } else if (imgRatio > maxRatio) {
+                imgRatio = maxWidth / actualWidth;
+                actualHeight = (int) (imgRatio * actualHeight);
+                actualWidth = (int) maxWidth;
+            } else {
+                actualHeight = (int) maxHeight;
+                actualWidth = (int) maxWidth;
+
+            }
+        }
+
+        options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight);
+        options.inJustDecodeBounds = false;
+        options.inDither = false;
+        options.inPurgeable = true;
+        options.inInputShareable = true;
+        options.inTempStorage = new byte[16 * 1024];
+
+        try {
+            bmp = BitmapFactory.decodeFile(imagePath, options);
+        } catch (OutOfMemoryError exception) {
+            exception.printStackTrace();
+
+        }
+        try {
+            scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.ARGB_8888);
+        } catch (OutOfMemoryError exception) {
+            exception.printStackTrace();
+        }
+
+        float ratioX = actualWidth / (float) options.outWidth;
+        float ratioY = actualHeight / (float) options.outHeight;
+        float middleX = actualWidth / 2.0f;
+        float middleY = actualHeight / 2.0f;
+
+        Matrix scaleMatrix = new Matrix();
+        scaleMatrix.setScale(ratioX, ratioY, middleX, middleY);
+
+        Canvas canvas = new Canvas(scaledBitmap);
+        canvas.setMatrix(scaleMatrix);
+        canvas.drawBitmap(bmp, middleX - bmp.getWidth() / 2, middleY - bmp.getHeight() / 2, new Paint(Paint.FILTER_BITMAP_FLAG));
+
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(imagePath);
+            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 0);
+            Matrix matrix = new Matrix();
+            if (orientation == 6) {
+                matrix.postRotate(90);
+            } else if (orientation == 3) {
+                matrix.postRotate(180);
+            } else if (orientation == 8) {
+                matrix.postRotate(270);
+            }
+            scaledBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 85, out);
+//        scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 40, out);
+
+        byte[] byteArray = out.toByteArray();
+
+        Bitmap updatedBitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+
+        return updatedBitmap;
+    }
+
+    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+        final float totalPixels = width * height;
+        final float totalReqPixelsCap = reqWidth * reqHeight * 2;
+
+        while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+            inSampleSize++;
+        }
+        return inSampleSize;
+    }
+}
+
---
+++ app/src/main/java/com/example/abhishekmadan/mypaint/activity/SplashScreenActivity.java
@@ -0,0 +1,51 @@
+package com.example.abhishekmadan.mypaint.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.abhishekmadan.mypaint.R;
+
+/**
+ * Simple Splash screen Activity
+ */
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private Animation mLinearAnim;
+
+    private Animation mSweepAnim;
+
+    private ImageView mBrushImageView;
+
+    private TextView mLineTextView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen_layout);
+        init();
+    }
+
+    public void init() {
+        mBrushImageView =   findViewById(R.id.brush_imageview);
+        mLineTextView =   findViewById(R.id.line_textview);
+        mLinearAnim = AnimationUtils.loadAnimation(this, R.anim.linear_move);
+        mSweepAnim = AnimationUtils.loadAnimation(this, R.anim.slide_right);
+        mLineTextView.setAnimation(mSweepAnim);
+        mBrushImageView.setAnimation(mLinearAnim);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, DrawingBoard.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 9000);
+    }
+}
